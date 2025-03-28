35a8c78a8620dad1e50a47292e3180f49f77d6bb
y0unj1NoH <nohyoong711@gmail.com>
2025-03-28 13:06:15 +0900

    chore: 에러 추가4

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
 function App() {
-  const [counttttt, setCount] = useState(0)
+  const [count, ssetCount] = useState(0)
 
   return (
     <>
